08caff321c88bedc60fe78d366b8854d75ede1dd
Michael Charles Aubrey <michael@akita-itlabo.com>
2022-01-20 01:24:34 +0900

    add template files for electron

--- sessions/2022-01-20/electron_template/src/main.js
+++ sessions/2022-01-20/electron_template/src/main.js
@@ -1,14 +1,17 @@
 const { app, BrowserWindow } = require("electron");
 const path = require('path');
 
-
-let mainWindow;
-
 app.on("ready", () => {
-  mainWindow = new BrowserWindow({
+  let mainWindow = new BrowserWindow({
     webPreferences: {
-      contextIsolation: false,
-      nodeIntegration: true,
+      /* 以下の2行のコメントを外すと、ウェブページのソース内で
+       nodeの機能（nodeのコアモジュールなど）を使えるようになります。
+       しかし、この方法は安全性が低く、レンダーウィンドウ内で一部の
+       コードがどのように実行されるかが変更されますので、この機能を
+       使用したい場合のみ、下記をコメントを外してください。 */
+       
+      // contextIsolation: false,
+      // nodeIntegration: true,
     },
   });
 
--- sessions/2022-01-20/electron_template/src/renderer/index.js
+++ sessions/2022-01-20/electron_template/src/renderer/index.js
@@ -1,31 +1,9 @@
-let i = 0;
-const colors = [
-  "#c62828",
-  "#6a1b9a",
-  "#283593",
-  "#00695c",
-  "#9e9d24",
-  "#ef6c00"
-]
+const state = {
+  count: 0
+}
 
-setInterval(() => {
-  console.log("Executed " + i)
-  const main = document.getElementById("main");
-  main.style.color = colors[i];
-  i = i + 1;
-  if (i >= colors.length) {
-    i = 0;
-  }
-}, 1000)
+const increment = () => {
+  state.count = state.count + 1;
 
-const fs = require('fs');
-
-const dirList = document.getElementById("dir-list")
-
-const dir = fs.readdirSync("./");
-
-dir.forEach(d => {
-  const li = document.createElement('li')
-  li.innerText = d;
-  dirList.appendChild(li);
-})
+  document.getElementById("count").innerText = state.count;
+}
